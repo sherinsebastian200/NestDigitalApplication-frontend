8fe6c772c8a11c279b280d7865f58f4216812d92
sherin200 <sherinsebastian21>
2022-12-13 12:48:40 +0530

    securitylogin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -17,7 +17,7 @@ export class AdminComponent {
     console.log(data)
   
     if (this.username=="admin"&&this.password=="12345") {
-     this.route.navigate(['/security'])
+     this.route.navigate(['/securitylogin'])
       
     } else {
       alert("failed")
